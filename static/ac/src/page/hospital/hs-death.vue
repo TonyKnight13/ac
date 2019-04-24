f53b3e49248a5de9eb586de2937fc4aa6242b2aa
Tony <289992302@qq.com>
2019-04-24 21:39:36 +0800

    殡葬界面修改

--- static/ac/src/page/hospital/hs-death.vue
+++ static/ac/src/page/hospital/hs-death.vue
@@ -5,9 +5,9 @@
     <div class="main-content-wrap">
       <img src="../../assets/images/gone/pexels-photo-1930851.jpg" alt="">
       <el-row :gutter="20" style="padding: 0.2rem 1rem; background:#ebebeb;">
-        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/new1.jpg" alt="">宠物公墓</el-col>
-        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/new1.jpg" alt="">宠物安乐死</el-col>
-        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/new1.jpg" alt="">宠物火化</el-col>
+        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/公墓.jpg" alt="">宠物公墓</el-col>
+        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/安乐死.jpg" alt="">宠物安乐死</el-col>
+        <el-col :span="8" style="padding: 0"><img src="../../assets/images/gone/玑珠.jpg" alt="">宠物玑珠</el-col>
       </el-row>
     </div>
     <!-- 底部 -->
@@ -31,9 +31,7 @@ export default {
 }
 </script>
 <style scoped>
-*{
-  padding: 0
-}
+
 .main-content-wrap{
   width: 100%;
   background: #ffffff
@@ -49,9 +47,11 @@ export default {
 .el-col{
   border: 1px solid #ebebeb;
   background:#ffffff;
-  height:3rem;
-  width:5.4rem;
-  margin: 0.1rem;
+  height:5.5rem;
+  width:5rem;
+  margin:0 0.3rem;
+  font-size: 0.32rem;
+  text-align: center;
 }
 .grid-content{
   width:100%;
@@ -59,6 +59,6 @@ export default {
 }
 .el-col img{
   width:100%;
-  height: 80%;
+  height: 88%;
 }
 </style>
