483a12802b92a22e289619a78c1349ebe6fbeb44
Tony <289992302@qq.com>
2019-05-14 14:50:19 +0800

    分页v1

--- models/pager.go
+++ models/pager.go
@@ -1,17 +1,34 @@
 package models
 
+import (
+	"github.com/astaxie/beego/orm"
+)
+
 type PageOptions struct {
 	Currentpage   int //当前页 ,默认1 每次分页,必须在前台设置新的页数,不设置始终默认1.在控制器中使用方式:cp, _ := this.GetInt("pno")   po.Currentpage = int(cp)
 	PageSize      int //页面大小
 	LinkItemCount int //生成标签的个数
 }
 
-func GetPageInfo(tableName string, currentpage int, pagesize int, conditions []string) (int int) {
+func GetPageInfo(tableName string, currentpage int, pagesize int, conditions string, optionClass []string,options []string) (int int) {
 	if currentpage <= 1 {
 		currentpage = 1
 	}
 	if pagesize == 0 {
-		pagesize = 20
+		pagesize = 10
 	}
 
+	o:= orm.NewOrm()
+	cond:=orm.NewCondition()
+
+	qs:=o.QueryTable(tableName)
+
+	optionClassCounts := len(itemsClass)
+
+	qs.Filter(itemClass[0],)
+
+	cond1 := cond.And(expr string, args ...interface{})
+
+
+	
 }
