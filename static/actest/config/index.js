3b19c2b3375465aa342b6c1a96c94e7ede347d99
Tony <289992302@qq.com>
2019-05-24 22:57:42 +0800

    merge

--- static/actest/config/index.js
+++ static/actest/config/index.js
@@ -8,10 +8,8 @@ module.exports = {
   dev: {
 
     // Paths
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '../../../static',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
